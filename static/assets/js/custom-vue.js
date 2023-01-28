ed249de5667f2f1810c4b6a017847036a2fe4617
QAM <qam600@gmail.com>
2023-01-28 12:48:07 +0400

    Vue Integrate

---
+++ static/assets/js/custom-vue.js
@@ -0,0 +1,13 @@
+
+const app = Vue.createApp({
+         data() {
+             return {
+               Adult:  1,
+               Children: 0,
+               Infant: 0,
+             
+             }
+           }
+     })
+app.mount("#canvas")
+
