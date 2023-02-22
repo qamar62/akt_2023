6e3999b2b8d8bdbdc82ca09ca44c48ee26e1f438
QAM <qam600@gmail.com>
2023-02-22 12:43:17 +0400

    outbound camptcha

--- outbounds/forms.py
+++ outbounds/forms.py
@@ -1,13 +1,14 @@
 
 from django import forms
 from .models import Inquiry
-
+from captcha.fields import ReCaptchaField
 
 
 
 
 class InquiryForm(forms.ModelForm):
+    captcha=ReCaptchaField()
     class Meta:
         model = Inquiry
-        fields = ['fullname', 'mobile', 'email', 'message']
+        fields = ['fullname', 'mobile', 'email', 'message','captcha']
 
